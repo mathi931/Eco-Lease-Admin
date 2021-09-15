672c60002e7e88319354551365ec60d005a9e697
mathi931 <mesi.mathi@gmail.com>
2021-09-15 17:06:02 +0200

    user data access commits

--- Data/Classes/UserDataAccess.cs
+++ Data/Classes/UserDataAccess.cs
@@ -11,68 +11,87 @@ namespace EcoLease_Admin.Data
 {
     class UserDataAccess : IUserHandler
     {
+        //gets and returns all users
         public List<User> GetAll()
         {
             try
             {
+                //open connection in try-catch with DataAccesHelper class to avoid connection string to be shown 
                 using (IDbConnection connection = new SqlConnection(ConString("EcoLeaseDB")))
                 {
+                    //runs and returns a list of users from the executed query
                     return connection.Query<User>($"SELECT * FROM Users").ToList();
                 }
             }
             catch (SqlException exp)
             {
+                //throws an error if the data access is unsucsessfull
                 throw new InvalidOperationException("Data could not be read", exp);
             }
         }
 
+        //inserts a new user
         public void Insert(User user)
         {
             try
             {
+                //query for insert a new user
                 var sql = @"INSERT INTO Users (firstName, lastName, dateOfBirth) 
                                    VALUES(@firstName, @lastName, @dateOfBirth)";
 
-
+                //open connection in try-catch with DataAccesHelper class to avoid connection string to be shown
                 using (IDbConnection connection = new SqlConnection(ConString("EcoLeaseDB")))
                 {
+                    //runs the query
                     connection.Execute(sql, user);
                 }
             }
             catch (SqlException exp)
             {
+                //throws an error if the data access is unsucsessfull
                 throw new InvalidOperationException("Data could not be write", exp);
             }
         }
 
+        //removes a user
         public void Remove(User user)
         {
             try
             {
-                var sql = @"DELETE FROM Users WHERE id = @id";
+                //query to remove a user by ID
+                var query = @"DELETE FROM Users WHERE id = @id";
+                //open connection in try-catch with DataAccesHelper class to avoid connection string to be shown
                 using (IDbConnection connection = new SqlConnection(ConString("EcoLeaseDB")))
                 {
-                    connection.Execute(sql, user);
+                    //runs the query
+                    connection.Execute(query, user);
                 }
             }
             catch (SqlException exp)
             {
+                //throws an error if the data access is unsucsessfull
                 throw new InvalidOperationException("Data could not be remove", exp);
             }
         }
 
+        //updates a user
         public void Update(User user)
         {
             try
             {
+                //query for update a user by id
                 var sql = @"update Users SET firstName = @firstName, lastName = @lastName, dateOfBirth = @dateOfBirth WHERE id = @id";
+
+                //open connection in try-catch with DataAccesHelper class to avoid connection string to be shown
                 using (IDbConnection connection = new SqlConnection(ConString("EcoLeaseDB")))
                 {
+                    //runs the query
                     connection.Execute(sql, user);
                 }
             }
             catch (SqlException exp)
             {
+                //throws an error if the data access is unsucsessfull
                 throw new InvalidOperationException("Data could not be update", exp);
             }
         }
