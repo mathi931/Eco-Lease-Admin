0ddc1ec5d59502e9340cd2e2838031609d5af447
mathi931 <mesi.mathi@gmail.com>
2021-09-22 16:58:30 +0200

    Status new model and access

---
+++ Data/Interfaces/IStatusHandler.cs
@@ -0,0 +1,14 @@
+﻿using EcoLease_Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoLease_Admin.Data.Interfaces
+{
+    interface IStatusHandler
+    {
+        List<Status> GetAll();
+    }
+}
---
+++ Models/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoLease_Admin.Models
+{
+    class Status
+    {
+        public int SId { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
